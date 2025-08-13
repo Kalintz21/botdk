edbcaa5e86e03ee997590ee1a26eab07b8236a95
Kalintz21 <polaquinho.exe@gmail.com>
2025-08-13 14:45:02 -0300

    index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["I'm the Boss! 👑"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Playing BF6 🔥"];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
